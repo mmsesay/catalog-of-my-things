640914df13a31678ac5a454a0afb43dc177a0bcc
mmsesay <maej_01@hotmail.com>
2022-01-04 12:39:28 +0000

    Add book.rb class and it method

---
+++ book.rb
@@ -0,0 +1,15 @@
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publisher, cover_state)
+    super()
+    @publisher = publisher
+    @cover_state = cover_state
+
+    @parent_is_archived = can_be_archived?
+  end
+
+  def can_be_archived?
+    @parent_is_archived || cover_state == 'bad'
+  end
+end
