78a82514911c555b52383844650cba945bf44e52
see-why <iyadicy@gmail.com>
2022-01-04 17:23:17 +0100

    Add musicAlbum class

---
+++ classes/music_album.rb
@@ -0,0 +1,17 @@
+require_relative './classes/item'
+
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+  attr_reader :items
+
+  def initialize(on_spotify, publish_date)
+    super(publish_date)
+    @id = Random.rand(1..1000)
+    @on_spotify = on_spotify
+    @items = []
+  end
+
+  def can_be_archived?
+    super.can_be_archived? && @on_spotify
+  end
+end
