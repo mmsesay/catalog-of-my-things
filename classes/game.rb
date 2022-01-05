2e41b713693700b44689c77ae445e43d37cf149c
HENRY-KC <henrykc24@yahoo.com>
2022-01-05 15:41:21 +0100

    Define can_be_archived method in Game class

--- classes/game.rb
+++ classes/game.rb
@@ -1,7 +1,7 @@
 require_relative './item'
 class Game < Item
-  def initialize(publish_date, last_played_at, multiplayer: false)
-    super(publish_date)
+  def initialize(published_date, last_played_at, multiplayer: false)
+    super(published_date)
     @last_played_at = last_played_at
     @multiplayer = multiplayer
   end
@@ -10,5 +10,13 @@ class Game < Item
 
   def add_author(author)
     !author.items.include?(self) && author.add_item(self)
+  end
+
+  def can_be_archived?
+    bool = super
+    now_year = Time.new.year
+    year_last_played = @last_played_at[:year]
+    archive = now_year - year_last_played
+    archive && bool
   end
 end
