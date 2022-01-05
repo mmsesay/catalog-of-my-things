5be1430982094156dc63f6df10b74c4bbca0a709
mmsesay <maej_01@hotmail.com>
2022-01-05 09:04:09 +0000

    Add tests for book class

---
+++ spec/books_spec.rb
@@ -0,0 +1,25 @@
+require './classes/book'
+
+describe Book do
+  context 'Book class tests' do
+    before(:each) do
+      @book = Book.new('John Doe', '2022/1/5', 'good')
+    end
+
+    it 'should return correct book publisher' do
+      expect(@book.publisher).to eq 'John Doe'
+    end
+
+    it 'should return correct book cover state' do
+      expect(@book.cover_state).to eq 'good'
+    end
+
+    it 'should return correct book publish_date' do
+      expect(@book.publish_date).to eq '2022/1/5'
+    end
+
+    it 'should return false unless @archived || cover_state == bad' do
+      expect(@book.can_be_archived?).to eq false
+    end
+  end
+end
