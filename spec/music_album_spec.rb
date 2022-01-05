981aedcf4acfb476700b033116f856ae41fd0fbc
see-why <iyadicy@gmail.com>
2022-01-05 13:24:02 +0100

    Fix test linter errors

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -4,8 +4,9 @@ require_relative '../classes/music_album'
 describe MusicAlbum do
   context 'MusicAlbum class Test' do
     on_spotify = true
+    name = '29'
     publish_date = Date.parse('2022/01/01')
-    album = MusicAlbum.new(on_spotify, publish_date)
+    album = MusicAlbum.new(on_spotify, name, publish_date)
 
     it 'is an instance of class item' do
       expect(album).to be_an_instance_of(MusicAlbum)
@@ -20,7 +21,8 @@ describe MusicAlbum do
     end
 
     new_date = Date.parse('1989/01/01')
-    new_album = MusicAlbum.new(on_spotify, new_date)
+    new_name = 'Kamakaze'
+    new_album = MusicAlbum.new(on_spotify, new_name, new_date)
 
     it 'checks can_be_archived as true' do
       expect(new_album.can_be_archived?).to eq(true)
